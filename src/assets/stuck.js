bda0fd8bc8e03fedbc39a035f60ff8d77219d5cc
Broderick Lemke <blemke4@gmail.com>
2025-04-23 09:52:43 -0500

    Create stuck.js

---
+++ src/assets/stuck.js
@@ -0,0 +1,457 @@
+const types = {
+    PLAY: "Play",
+    TIW: "The Infinite Wrench"
+}
+
+const authors = {
+    NEO_ABBY: "Abby Pajakowski",
+    NEO_ALE: "Ale Ramirez",
+    NEO_ANDRES: "Andres Coronado",
+    NEO_ANNIE: "Annie Share",
+    NEO_AUSSIE: "aussie b",
+    NEO_CAT: "Cat Huck",
+    NEO_CONNOR: "Connor Shioshita Pickett",
+    NEO_DAN: "Dan Keyy-Hobert",
+    NEO_EMMA: "Emma Casey",
+    NEO_JASMINE: "Jasmine Henri Jordan",
+    NEO_JJ: "JJ",
+    NEO_KR: "KR Riiber",
+    NEO_NEIL: "Neil Bhandari",
+    NEO_NIKY: "Niky Crawford",
+    NEO_UNKNOWN: "The Infinite Wrench - Unknown Neo",
+    NEO_COLLAB: "The Infinite Wrench - Collaboration, See Notes",
+}
+
+let stuckData = [
+    {
+        "Inspiration": "How to Be Cool",
+        "Type": types.PLAY,
+        "Challenges": [
+            "Change where the audience is sitting",
+            "Make the audience uncomfortable (safely)",
+            "Come back to the same piece of text",
+            "Have someone else read your words",
+            "Destroy/change your program/menu",
+            "Keep notes from show to show",
+            "Get something wet, with something other than water",
+            "Share something you wrote when younger",
+            "Have an audience member read text",
+            "Question an ideal you've held a long time and how you've pursued it",
+            "Endurance",
+            "Oppressively bright light",
+            "Interruptions",
+            "Burn something",
+            "Enter from somewhere weird",
+            "Build more stage, abuse it",
+            "Change clothes",
+            "What idols do/have you had?",
+            "Change through time",
+            "Same through time",
+            "Long for something intensely, even if you don't know how to get it",
+            "Admit you were wrong"
+        ],
+        "Date": "February 2025",
+        "Notes": "",
+        "Author": authors.NEO_NEIL,
+    },
+    {
+        "Inspiration": "Oranges do not occur naturally",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Never address the title",
+            "Throw something",
+            "Say something gross, and re-reference it later",
+            "Time a task to take exactly as long as your text",
+            "Get sticky",
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_ANNIE,
+    },
+    {
+        "Inspiration": "ALL HORSES ARE WILD HORSES EVERY HORSE IS A WILD HORSE",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Playground Chant",
+            "Find a way to mimic an animal or object with your body",
+            "Speak to a steady tempo",
+            "Face away from someone",
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "89 Seconds Until Midnight",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Find power in a line",
+            "Tiny repeated movements, faster. faster.",
+            "Break the rules of the show",
+            "Compete with others, does the winner really win?",
+            "All vs One",
+            "Crowd around something",
+            "TikTok Dance",
+            "Acknowledge progress amongst the failures",
+            "A simple head turn when speaking",
+            "A sequel - What comes next?"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "Play with a sequel play",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "The art of theater and its forms",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Use an object from childhood",
+            "Don't explain the rules to the audience",
+            "Debate, but not really",
+            "Allow change performance to performance",
+            "Call someone out",
+            "Don't remember your own rules, have fun",
+            "Question art",
+            "How do you perform?",
+            "Bow. Do it now, not at the end of the show. Now.",
+            "A highschool wrestling meet",
+            "Stop task. Share story.",
+            "Play with everyone you can",
+
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "I THINK this is an Andres play because they were consistently in it",
+        "Author": authors.NEO_ANDRES,
+    },
+    {
+        "Inspiration": "SYNTHESIS",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Don't talk to the crowd, just one person, only to instruct",
+            "Mitosis",
+            "Give, destroy, swap",
+            "Elbow choreography",
+            "Teach, do, teach, do, teach, do.",
+            "Use someone's name",
+            "The more people do something, the less weird it is"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "Head Chef",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Be cheeky to the audience, drag out something they don't want",
+            "It's going to be gross, but dive in fully",
+            "Make it a guessing game, never learn the right answer if you're wrong",
+            "Season it,  liberally.",
+            "Let your castmates pick, no option is good",
+            "You know what's happening over there even if you can't see it"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_ANNIE,
+    },
+    {
+        "Inspiration": "NONE OF OUR OWN STUNTS",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Use an object to stand in for yourself",
+            "You HAVE to wear this specific outfit tonight",
+            "Dream bigger than you could do, find a way to approximate it",
+            "Scream over music",
+            "Be a little bit dangerous, just the tiniest teensiest bit",
+            "So many props, please get the choreo right",
+            "FORM: A B C B A B C B A B C B A B C B D E",
+            "Do a flip off a ladder, come on, it isn't that hard."
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "Andres and Annie Play",
+        "Author": NEO_COLLAB,
+    },
+    {
+        "Inspiration": "breathing is considered both voluntary and involuntary",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "You know what this is going to be from the title",
+            "Video that is better watched with a group",
+            "Take a break and sit with the audience, get into their shoes",
+            "Abuse a bodily function",
+            "The punchline isn't anything more than a rhythm change",
+            "Nothing more than instructions, no stories, no facts, to opinions, just instruction."
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "how to say i'm sorry",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Take so much time. You don't need to rush. Rushing ruins it. Someone will appreciate the slow methodical nature, even if it's just you",
+            "Metaphor, no words",
+            "Big setup, with big objects",
+            "One light for most, then a second at the end.",
+            "Go back and do it right",
+            "What happens if this fails?",
+            "You do 90% of the work, someone does 10%",
+            "Let the music do some work for you",
+            "Far away, then close to someone"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_ANNIE,
+    },
+    {
+        "Inspiration": "Pre-Dawn Chorus / Post-Dusk Chorus",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Spread throughout the audience",
+            "Lights slowly come up and down through the play",
+            "Are you speaking to the audience or the cast?",
+            "There are so many words coming out of your mouth, and everyone's mouths, and then just one person is left speaking",
+            "Pause... pause.... callback",
+            "I'm X Are you X?",
+            "Use various voices as an echo",
+            "Empty stage",
+            "Sequel, in reverse, mostly"
+
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "Sestina for Couch-Surfing",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Write a sestina (poem form)",
+            "God mic, castmates do a task",
+            "Public transit",
+            "Butchered quote, take vibes only: The dead bird on the sidewalk is poetry, the dead rat is what's left.",
+            "Chalk can be washed off, go everywhere with it",
+            "Start in the same place, take wildly different paths",
+            "Stop an action all at the same time, and leave",
+            "Be hyper aware of teh space you're in, enjoy what makes it unique tonight",
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_ANNIE,
+    },
+    {
+        "Inspiration": "BRICK",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Oh god, are you really going to hurt yourself like that?",
+            "Yeah, do it again, you're not sweating enough",
+            "Give others a moment of comedy while you're busy",
+            "The audience knows what this feels like, they're antsy",
+            "Brick. Brick Brick. Brick Brick Brick. Brick Brick Brick Brick. Brick Brick Brick Brick Brick.",
+            "Try to craft a line someone will be repeating at home",
+            "Faster. Faster. Faster. STOP. SLOW. Repeat.",
+            "Try to push yourself to do it, you won't, but step close to that line each time."
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_CONNOR,
+    },
+    {
+        "Inspiration": "there is no I in ensemble (but if there was, it would be ensImble.)",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Together, then break off one at a time and come back",
+            "Spin at varying heights",
+            "Give the audience a dare",
+            "Nothing can ever be done the same way twice...?",
+            "Lecture the crowd, they need to hear it",
+            "Defeat facism with your eyes closed",
+            "Wait, that can happen again? I thought it was a one time thing?"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "Not entirely sure on the author, but I suspect Connor based on the second half of the play.",
+        "Author": authors.NEO_CONNOR,
+    },
+    {
+        "Inspiration": "A Lesson (a dream) in the Second Person",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Make the stage a mess, a danger even",
+            "Put your words in someone else's mouth",
+            "Practical effects, make it otherworldly",
+            "HA! A great reveal! I was in this piece all along!",
+            "You just stand there and do what I tell you to",
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_ANDRES,
+    },
+    {
+        "Inspiration": "for lent, i'm giving up...",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Wave a giant flag",
+            "Give up on something, commit to us",
+            "Play with a turn of phrase from the season",
+            "Each person sings one line, ocassionally they double up",
+            "Reverse the course you've taken two thirds through the piece, but don't cheap out",
+            "People gathered tightly around a guitar is a powerful thing",
+            "Change the timbre of your voice"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "aussie might be the author, but I'm unsure",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "We are all James Carville tonight (James Carville is a Democratic consultant who insisted that Kamala Harris simply couldn't lose, who advised Bill Clinton to move right on crime, race, and drugs, and who, last week in the New York Times, advised the Democratic party not to fight, not to obstruct or resist the plunder and demolition of the federal government because now is not the time, he literally said 'roll over and play dead' he said 'instead of gearing up to fight them the most radical thing we can do is nothing at all,' and this guy is on TV, he's in the newspapers, and he's far from the only one, he's just the most senior representative of an entire class of comfortable dullards with nothing at stake and nothing to add and he's the one i h8 the most",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Exposition in the title",
+            "Bodies on stage, only one person talking",
+            "Abbreviation after being verbose",
+            "Don't listen to the audience (for a reason)",
+            "Choices don't matter",
+            "Conduct a survey"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "Possibly a Connor play?",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "AMEN IS THE NEW NEXT",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Let the season change the format of your work",
+            "You need to remember this so many times, and you're going to struggle",
+            "Introduce a mechanic without explicitly stating how it works",
+            "Short but it changes everything."
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_AUSSIE,
+    },
+    {
+        "Inspiration": "Every Day We Invent the Wheel",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Play with your childhood imagination",
+            "The sun comes up and goes back down",
+            "We have the tools to do this, but we still don't to use them",
+            "There are problems, and I know how to fix them",
+            "Everything is a cycle, we will crave what we had in the past",
+            "Your childhood was different than now",
+            "We are all very close together, there is a boundary keeping us close"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "interdependence",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "Rely on others to support you",
+            "Everyone can be involved in this piece even if they might struggle with parts",
+            "Use your core, let it keep you stable, let it keep others stable",
+            "Lighting change as something falls through the air",
+            "Let removing objects from the stage be a cause for cheering",
+            "Find meaning in old trends"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_UNKNOWN,
+    },
+    {
+        "Inspiration": "it's calving season",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "I am like those that come before me",
+            "Correct yourself when you get it wrong",
+            "Get exhausted while being still",
+            "It sounds like _____",
+            "You're held your tongue too long and you need to speak your truth, at least here you do",
+            "It's not fair, but at least I have options",
+            "I am _____ here, I cannot be ______ there.",
+            "Take an action that has a purpose and deprive it of that purpose"
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": authors.NEO_CAT,
+    },
+    {
+        "Inspiration": "",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "",
+            ""
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": "",
+    },
+    {
+        "Inspiration": "",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "",
+            ""
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": "",
+    },
+    {
+        "Inspiration": "",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "",
+            ""
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": "",
+    },
+    {
+        "Inspiration": "",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "",
+            ""
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": "",
+    },
+    {
+        "Inspiration": "",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "",
+            ""
+        ],
+        "Date": "In show: 3/14/25",
+        "Notes": "",
+        "Author": "",
+    },
+
+
+
+]
+
+
+
+export default stuckData;
+
+/*
+    {
+        "Inspiration": "",
+        "Type": "The Infinite Wrench",
+        "Challenges": [
+            "",
+            ""
+        ],
+        "Date": "",
+        "Notes": "",
+        "Author": "",
+    },
+*/
