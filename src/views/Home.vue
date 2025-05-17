f0b93acff2fa0c228d167a2d42cd3f3ac811223c
Broderick Lemke <blemke4@gmail.com>
2025-05-17 15:37:38 -0500

    Home page update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,80 @@
+<script setup>
+import { ref, reactive, watch  } from "vue";
+import { useRouter } from 'vue-router'
+const router = useRouter();
+
+const password = ref("");
+const unlocked = ref(false);
+const showPass = ref(false);
+
+class Page {
+    constructor(name, author, pass, route) {
+        this.name = name;
+        this.author = author;
+        this.route = route;
+        this.pass = pass;
+    }
+}
+
+const authors = {
+    BroderickLemke: "Broderick Lemke",
+    OliviaSoliz: "Olivia Soliz"
+}
+
+const pages = reactive([
+    new Page("_unity", authors.BroderickLemke, ["unity", "_unity"], "/unity"),
+    new Page("30F. Non Smoker, Social Drinker. 5’2” Bisexual. ",authors.OliviaSoliz, ["thirtyf", "30f"],  "/thirtyf"),
+    new Page("Playwriting Idea Generator", authors.BroderickLemke, ["inspiration", "stuck"], "/stuck"),
+])
+
+
+watch(password, async (newPass, oldPass) => {
+    if(newPass.toLowerCase() == "insecure"){
+        unlocked.value = true;
+    } 
+    
+    pages.forEach(page => {
+        if(Array.isArray(page.pass)) {
+            page.pass.forEach(pass => {
+                if(newPass.toLowerCase() == pass) {
+                    router.push(page.route)
+                }    
+            })
+        } else {
+            if(newPass.toLowerCase() == page.pass) {
+                    router.push(page.route)
+            } 
+        } 
+    });
+})
+
+
+
+
+
+</script>
+
 <template>
-    <div>
-        Home!
-    </div>
-  </template>
-  
-  <style scoped></style>
+  <div class="p-5">
+        <div class="row bg-lime-lighten-5 rounded elevation-3 p-2">
+            <h2 class="my-2">Digital Theater Hub! <v-icon icon="rebase_edit" class="" /></h2>
+            
+            
+            <v-text-field  v-model="password"
+            :append-icon="showPass ? 'visibility' : 'visibility_off'"
+            :type="showPass ? 'text' : 'password'"
+            name="input-10-1"
+            @click:append="showPass = !showPass" ></v-text-field>
+
+            <div v-if="unlocked">
+               <h3>Navigation:</h3>
+                <v-data-table :items="pages"></v-data-table>
+
+            </div>
+        </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
